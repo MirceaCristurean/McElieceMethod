0b7672fc077ad8d1cc0380f13f876d4620638c75
Mircea Cristurean <cristurean.mircea@yahoo.com>
2023-10-31 12:12:29 +0200

    - organized code - removed warnings

--- matrix.py
+++ matrix.py
@@ -1,30 +1,33 @@
-from random import shuffle, randrange
+from random import shuffle
 import numpy as np
 from time import time
-import sys
+
 timeout = time() + 3  # seconds
 
-def random_vector(length, w):
-    arr = [True]*w+[False]*(length-w)
+
+def random_vector(length, weight):
+    arr = [True] * weight + [False] * (length - weight)
     shuffle(arr)
     return np.array(arr, dtype='bool')
 
-def generate_matrix_h(rows, columns, w):
-    # initializare matrice cu zero binar
+
+def generate_matrix_h(rows, columns, weight):
+    # initialize matrix with 0 binary
     matrix = np.zeros((rows, columns), dtype='bool')
     for index in range(0, rows):
-        new_row = [True]*w+[False]*(columns-w)
+        new_row = [True] * weight + [False] * (columns - weight)
         shuffle(new_row)
         matrix[index] = list(new_row)
-    print_matrix(matrix)
     return matrix
+
 
 def get_rank(m):
     return np.linalg.matrix_rank(m)
 
+
 def print_matrix(matrix, binary=True):
     if binary:
-        matrix = matrix*1
+        matrix = matrix * 1
         for row in matrix:
             print(row)
     else:
@@ -32,81 +35,86 @@ def print_matrix(matrix, binary=True):
             print(row)
     print()
 
+
 def print_rank(matrix):
     print("The rank is ", np.linalg.matrix_rank(matrix))
 
+
 def remove_duplicates_rows(matrix):
-    H_final = np.copy(matrix)
-    print_matrix(matrix)
-    delta = n-k-get_rank(matrix)
+    h_final = np.copy(matrix)
+    delta = n - k - get_rank(matrix)
     if delta != 0:
-        H_temp = np.copy(matrix)
-        while (delta > 0):
-            for column in range(0, len(H_temp)):
-                H_temp = np.delete(H_temp, column, 0)
-                print_matrix(H_temp)
-                if (get_rank(matrix) == get_rank(H_temp)):
-                    matrix = np.copy(H_temp)
-                    H_final = np.copy(H_temp)
+        h_temp = np.copy(matrix)
+        while delta > 0:
+            for column in range(0, len(h_temp)):
+                h_temp = np.delete(h_temp, column, 0)
+                if get_rank(matrix) == get_rank(h_temp):
+                    matrix = np.copy(h_temp)
+                    h_final = np.copy(h_temp)
                 break
             else:
-                H_temp = np.copy(matrix)
+                h_temp = np.copy(matrix)
         delta = delta - 1
     else:
         print('Rank full')
-    print_matrix(H_final)
-    return H_final
+    print("Matrix without duplicate rows")
+    print_matrix(h_final)
+    return h_final
 
-def calcul_Lscor(matrix, sindrome):
-    LS, = sindrome.nonzero()
-    Lscor = []
+
+def calculate_score(matrix, syndrome):
+    ls, = syndrome.nonzero()
+    l_score = []
     for index in range(0, n):
-        nonzeros, = matrix[:, index].nonzero()
-        intersect = np.intersect1d(nonzeros, LS)
-        Lscor.append(len(intersect))
-    print("Scor", Lscor)
-    return Lscor
+        nonzero, = matrix[:, index].nonzero()
+        intersect = np.intersect1d(nonzero, ls)
+        l_score.append(len(intersect))
+    print("score", l_score)
+    return l_score
 
-def decodaj(matrix, sindrome, e):
+
+def decode(matrix, syndrome, e):
     y = np.zeros(n, dtype="bool")
-    length, = sindrome.nonzero()
+    length, = syndrome.nonzero()
+    print("length", length)
     while len(length) != 0:
-        # print(length)
-        scor = calcul_Lscor(matrix, sindrome)
-        M = max(scor)
-        ListMax, = (np.array(scor) >= M).nonzero()
-        V = np.zeros(n-k, dtype="bool")
+        score = calculate_score(matrix, syndrome)
+        m = max(score)
+        list_max, = (np.array(score) >= m).nonzero()
+        v = np.zeros(n - k, dtype="bool")
         x = np.zeros(n, dtype="bool")
-        for column in ListMax:
-            V = V + np.transpose(matrix)[:][column]
+        for column in list_max:
+            v = v + np.transpose(matrix)[:][column]
             x[column] = 1
-        sindrome = np.logical_xor(sindrome, V)
+        syndrome = np.logical_xor(syndrome, v)
         y = y + x
-        length, = sindrome.nonzero()
+        length, = syndrome.nonzero()
         if time() > timeout:
             return False
-    return np.array_equal(e,y)
+    return np.array_equal(e, y)
 
 
 if __name__ == '__main__':
     validity = []
-    n = 200  # coloane
-    k = 100   # n-k randuri
-    w = 5  # numar de 1 pe linie
-    errorWeight = 3 # complexitatea vectorului de eroare
-    generated_matrix = generate_matrix_h(n-k, n, w) # generare aleatorie matrice
-    striped_matrix = remove_duplicates_rows(generated_matrix) # curatare matrice => matricea generatoare H
-    for errorWeight in range(3, 4):
-        true_contor = 0
-        for i in range(1,2):
-            print(f"Pondere: {errorWeight}, i: {i}")
-            e = random_vector(n, errorWeight) # vectorul de eroare de pondere errorWeight
-            S = striped_matrix @ e # calcul sindrom
-            sindrome = np.copy(S)
-            print(decodaj(striped_matrix, sindrome, e))
-            if (decodaj(striped_matrix, sindrome, e)) is True:
-                true_contor += 1
-        result = true_contor/5
+    n = 20  # columns
+    k = 10  # n-k rows
+    w = 5  # number of 1 value on a row
+    errorWeight = 3  # complexity of error vector
+    generated_matrix = generate_matrix_h(n - k, n, w)  # generating random matrix
+    striped_matrix = remove_duplicates_rows(generated_matrix)  # cleaning matrix => generator matrix H
+    start_error_weight = 3
+    stop_error_weight = 4
+    number_of_retries = 2
+    for errorWeight in range(start_error_weight, stop_error_weight):
+        true_count = 0
+        for i in range(1, number_of_retries):
+            print(f"Weight: {errorWeight}, i: {i}")
+            error_vector = random_vector(n, errorWeight)  # error vector
+            S = striped_matrix @ error_vector  # calculating syndrome
+            calculated_syndrome = np.copy(S)
+            print(decode(striped_matrix, calculated_syndrome, error_vector))
+            if (decode(striped_matrix, calculated_syndrome, error_vector)) is True:
+                true_count += 1
+        result = true_count / number_of_retries
         validity.append(result)
     print(validity)
-
